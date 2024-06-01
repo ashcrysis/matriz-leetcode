ad869eac12b4f913a311ca3ed3cb391a044ff94b
asher gabriela <109311931+ashcrysis@users.noreply.github.com>
2024-06-01 01:03:08 -0300

    on pokemon click also shows the pokemon type background

--- projects/pokeapi-ash/src/components/render.jsx
+++ projects/pokeapi-ash/src/components/render.jsx
@@ -1,8 +1,9 @@
 import React, { useState, useEffect } from "react";
 import "../App.css";
-
+import defaultBg from "../poke-bgs/normal.png";
 const Render = ({ name, types, image, height, weight }) => {
   const [description, setDescription] = useState("Loading...");
+  const [bgImage, setBgImage] = useState(null);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -28,41 +29,30 @@ const Render = ({ name, types, image, height, weight }) => {
     };
 
     fetchData();
-  }, [name]);
 
-  const findEnglishDescription = (speciesData) => {
-    if (
-      speciesData &&
-      speciesData.flavor_text_entries &&
-      Array.isArray(speciesData.flavor_text_entries)
-    ) {
-      const englishDescription = speciesData.flavor_text_entries.find(
-        (entry) => entry.language.name === "en"
-      );
-      return englishDescription
-        ? englishDescription.flavor_text
-        : "English description not found.";
-    } else {
-      return "English description not found.";
-    }
-  };
-
+    import(`../poke-bgs/${types[0]}.png`)
+      .then((image) => setBgImage(image.default))
+      .catch((error) => {
+        console.error("Error importing image:", error);
+        setBgImage(defaultBg);
+      });
+  }, [name, types]);
   const displayTypes = types.slice(0, 2);
-
   return (
     <div id="renderDiv" role="region" aria-labelledby="pokemon-name">
       <div id="pokeDataHolder">
         <div>
           <h1 id="pokemon-name">{capitalizeFirstLetter(name)}</h1>
+          {bgImage && <img id="pokebg" src={bgImage} alt="" />}
           <img id="pokeImage" src={image} alt={name} />
-          <p>Types: {capitalizeFirstLetter(displayTypes.join(", "))}</p>
+          <p id="typepoke">
+            Types: {capitalizeFirstLetter(displayTypes.join(", "))}
+          </p>
           <div id="pokeHW">
             <p>Height: {height} m</p>
             <p>|</p>
             <p>Weight: {weight} kg</p>
           </div>
-          <div id="pokeAbilities"></div>
-          <div id="pokeStats"></div>
           <div id="pokeDescription">
             <h3>Description:</h3>
             <p>{description}</p>
@@ -75,6 +65,22 @@ const Render = ({ name, types, image, height, weight }) => {
 
 export const capitalizeFirstLetter = (string) => {
   return string.charAt(0).toUpperCase() + string.slice(1);
+};
+const findEnglishDescription = (speciesData) => {
+  if (
+    speciesData &&
+    speciesData.flavor_text_entries &&
+    Array.isArray(speciesData.flavor_text_entries)
+  ) {
+    const englishDescription = speciesData.flavor_text_entries.find(
+      (entry) => entry.language.name === "en"
+    );
+    return englishDescription
+      ? englishDescription.flavor_text
+      : "English description not found.";
+  } else {
+    return "English description not found.";
+  }
 };
 
 export default Render;
