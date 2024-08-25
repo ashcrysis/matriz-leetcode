9759fc4581b73499b2e58bac2e5d52057179359e
j0jds <mrlucassilva7342@gmail.com>
2024-08-25 00:00:42 -0300

    Changing App.tsx

--- matriz/src/App.tsx
+++ matriz/src/App.tsx
@@ -1,14 +1,7 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Message from './Message'
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-      <div/>
-  )
+  return <div><Message /></div>
 }
 
-export default App
+export default App;
