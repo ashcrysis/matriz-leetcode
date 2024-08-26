9382212ab8aef0b762c7c7da0dfc5a977a538bd9
j0jds <mrlucassilva7342@gmail.com>
2024-08-25 23:49:35 -0300

    Testing interpolation with dynamic name

--- matriz/src/Message.tsx
+++ matriz/src/Message.tsx
@@ -2,7 +2,10 @@
 
 function Message() {
     // JSX: Javascript XML
-    return <h1>Hello World!</h1>;
+    const name = 'Luinhas';
+    if (name)
+        return <h1>Hello { name }!</h1>;
+    return <h1>Hello User!</h1>
 }
 
 export default Message;
