c2353cee7916b863e8e3a76c1b0e658a219beac2
Xnor <lucas.silva@direction.com.br>
2024-09-10 21:50:39 -0300

    Consuming API directly and returning response data

--- luinhas/src/Pokeservice.js
+++ luinhas/src/Pokeservice.js
@@ -2,5 +2,5 @@ import axios from "axios";
 
 export async function getPokemons() {
    const response = await axios.get("https://pokeapi.co/api/v2/pokemon");
-   return response.data;
+   return response.data.results;
 }
