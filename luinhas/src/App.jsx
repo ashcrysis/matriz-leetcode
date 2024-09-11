d166e8dcd59533ae127db876b2cf29ea262198ce
Xnor <lucas.silva@direction.com.br>
2024-09-10 21:49:32 -0300

    Using API and iteration for exhibit

--- luinhas/src/App.jsx
+++ luinhas/src/App.jsx
@@ -1,14 +1,13 @@
-import { useState } from 'react'
 import './App.css'
 import { useEffect, useState } from 'react'
 import { getPokemons } from './Pokeservice'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [pokemons, setPokemons] = useState([])
 
   useEffect (() => {
     getPokemons()
-    .then()
+    .then(data => setPokemons(data.map(d => d.name)));
   },[])
 
   return (
@@ -16,6 +15,7 @@ function App() {
     <div className='primeiro'>
       <h1>Bem-vindo a Pokéapi Luinhas !</h1>
     </div>
+    { pokemons.map(p => <p>{p}</p>) }
     </>
   )
 }
